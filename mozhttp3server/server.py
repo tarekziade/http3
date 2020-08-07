44ba6568ac2c0f09c666dc1599d4cf256d909209
Tarek Ziade <tarek@ziade.org>
2020-08-07 14:16:00 +0200

    fixed badly downloaded sources

--- mozhttp3server/server.py
+++ mozhttp3server/server.py
@@ -1,162 +1,45 @@
+#!/usr/bin/python3
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import argparse
 import asyncio
 import importlib
 import logging
-import time
-from collections import deque
-from email.utils import formatdate
-from typing import Callable, Deque, Dict, List, Optional, Union, cast
+import pathlib
 
-import wsproto
-import wsproto.events
-from quic_logger import QuicDirectoryLogger
-
-import aioquic
-from aioquic.asyncio import QuicConnectionProtocol, serve
+from aioquic.asyncio import serve
+from aioquic.quic.configuration import QuicConfiguration
 from aioquic.h0.connection import H0_ALPN, H0Connection
 from aioquic.h3.connection import H3_ALPN, H3Connection
 from aioquic.h3.events import DataReceived, H3Event, HeadersReceived
-from aioquic.h3.exceptions import NoAvailablePushIDError
-from aioquic.quic.configuration import QuicConfiguration
-from aioquic.quic.events import DatagramFrameReceived, ProtocolNegotiated, QuicEvent
-from aioquic.tls import SessionTicket
+from typing import Callable, Union
+
+from mozhttp3server.quic_logger import QuicDirectoryLogger
+from mozhttp3server.aioquic_example_server import (
+    HttpConnection,
+    HttpRequestHandler,
+    HttpServerProtocol,
+    SessionTicketStore,
+    WebSocketHandler
+)
 
 try:
     import uvloop
 except ImportError:
+    print("uvloop is not available")
     uvloop = None
 
 AsgiApplication = Callable
-HttpConnection = Union[H0Connection, H3Connection]
-
-SERVER_NAME = "aioquic/" + aioquic.__version__
+HERE = pathlib.Path(__file__).parent
 
 
-class HttpRequestHandler:
-    def __init__(
-        self,
-        *,
-        authority: bytes,
-        connection: HttpConnection,
-        protocol: QuicConnectionProtocol,
-        scope: Dict,
-        stream_ended: bool,
-        stream_id: int,
-        transmit: Callable[[], None],
-    ) -> None:
-        self.authority = authority
-        self.connection = connection
-        self.protocol = protocol
-        self.queue: asyncio.Queue[Dict] = asyncio.Queue()
-        self.scope = scope
-        self.stream_id = stream_id
-        self.transmit = transmit
-
-        if stream_ended:
-            self.queue.put_nowait({"type": "http.request"})
-
-    def http_event_received(self, event: H3Event) -> None:
-        if isinstance(event, DataReceived):
-            self.queue.put_nowait(
-                {
-                    "type": "http.request",
-                    "body": event.data,
-                    "more_body": not event.stream_ended,
-                }
-            )
-        elif isinstance(event, HeadersReceived) and event.stream_ended:
-            self.queue.put_nowait(
-                {"type": "http.request", "body": b"", "more_body": False}
-            )
-
+class MozHttpRequestHandler(HttpRequestHandler):
     async def run_asgi(self, app: AsgiApplication) -> None:
         await application(self.scope, self.receive, self.send)
 
-    async def receive(self) -> Dict:
-        return await self.queue.get()
 
-    async def send(self, message: Dict) -> None:
-        if message["type"] == "http.response.start":
-            self.connection.send_headers(
-                stream_id=self.stream_id,
-                headers=[
-                    (b":status", str(message["status"]).encode()),
-                    (b"server", SERVER_NAME.encode()),
-                    (b"date", formatdate(time.time(), usegmt=True).encode()),
-                ]
-                + [(k, v) for k, v in message["headers"]],
-            )
-        elif message["type"] == "http.response.body":
-            self.connection.send_data(
-                stream_id=self.stream_id,
-                data=message.get("body", b""),
-                end_stream=not message.get("more_body", False),
-            )
-        elif message["type"] == "http.response.push" and isinstance(
-            self.connection, H3Connection
-        ):
-            request_headers = [
-                (b":method", b"GET"),
-                (b":scheme", b"https"),
-                (b":authority", self.authority),
-                (b":path", message["path"].encode()),
-            ] + [(k, v) for k, v in message["headers"]]
-
-            # send push promise
-            try:
-                push_stream_id = self.connection.send_push_promise(
-                    stream_id=self.stream_id, headers=request_headers
-                )
-            except NoAvailablePushIDError:
-                return
-
-            # fake request
-            cast(HttpServerProtocol, self.protocol).http_event_received(
-                HeadersReceived(
-                    headers=request_headers, stream_ended=True, stream_id=push_stream_id
-                )
-            )
-        self.transmit()
-
-
-class WebSocketHandler:
-    def __init__(
-        self,
-        *,
-        connection: HttpConnection,
-        scope: Dict,
-        stream_id: int,
-        transmit: Callable[[], None],
-    ) -> None:
-        self.closed = False
-        self.connection = connection
-        self.http_event_queue: Deque[DataReceived] = deque()
-        self.queue: asyncio.Queue[Dict] = asyncio.Queue()
-        self.scope = scope
-        self.stream_id = stream_id
-        self.transmit = transmit
-        self.websocket: Optional[wsproto.Connection] = None
-
-    def http_event_received(self, event: H3Event) -> None:
-        if isinstance(event, DataReceived) and not self.closed:
-            if self.websocket is not None:
-                self.websocket.receive_data(event.data)
-
-                for ws_event in self.websocket.events():
-                    self.websocket_event_received(ws_event)
-            else:
-                # delay event processing until we get `websocket.accept`
-                # from the ASGI application
-                self.http_event_queue.append(event)
-
-    def websocket_event_received(self, event: wsproto.events.Event) -> None:
-        if isinstance(event, wsproto.events.TextMessage):
-            self.queue.put_nowait({"type": "websocket.receive", "text": event.data})
-        elif isinstance(event, wsproto.events.Message):
-            self.queue.put_nowait({"type": "websocket.receive", "bytes": event.data})
-        elif isinstance(event, wsproto.events.CloseConnection):
-            self.queue.put_nowait({"type": "websocket.disconnect", "code": event.code})
-
+class MozWebSocketHandler(WebSocketHandler):
     async def run_asgi(self, app: AsgiApplication) -> None:
         self.queue.put_nowait({"type": "websocket.connect"})
 
@@ -166,63 +49,11 @@ class WebSocketHandler:
             if not self.closed:
                 await self.send({"type": "websocket.close", "code": 1000})
 
-    async def receive(self) -> Dict:
-        return await self.queue.get()
 
-    async def send(self, message: Dict) -> None:
-        data = b""
-        end_stream = False
-        if message["type"] == "websocket.accept":
-            subprotocol = message.get("subprotocol")
-
-            self.websocket = wsproto.Connection(wsproto.ConnectionType.SERVER)
-
-            headers = [
-                (b":status", b"200"),
-                (b"server", SERVER_NAME.encode()),
-                (b"date", formatdate(time.time(), usegmt=True).encode()),
-            ]
-            if subprotocol is not None:
-                headers.append((b"sec-websocket-protocol", subprotocol.encode()))
-            self.connection.send_headers(stream_id=self.stream_id, headers=headers)
-
-            # consume backlog
-            while self.http_event_queue:
-                self.http_event_received(self.http_event_queue.popleft())
-
-        elif message["type"] == "websocket.close":
-            if self.websocket is not None:
-                data = self.websocket.send(
-                    wsproto.events.CloseConnection(code=message["code"])
-                )
-            else:
-                self.connection.send_headers(
-                    stream_id=self.stream_id, headers=[(b":status", b"403")]
-                )
-            end_stream = True
-        elif message["type"] == "websocket.send":
-            if message.get("text") is not None:
-                data = self.websocket.send(
-                    wsproto.events.TextMessage(data=message["text"])
-                )
-            elif message.get("bytes") is not None:
-                data = self.websocket.send(
-                    wsproto.events.Message(data=message["bytes"])
-                )
-
-        if data:
-            self.connection.send_data(
-                stream_id=self.stream_id, data=data, end_stream=end_stream
-            )
-        if end_stream:
-            self.closed = True
-        self.transmit()
+Handler = Union[MozHttpRequestHandler, MozWebSocketHandler]
 
 
-Handler = Union[HttpRequestHandler, WebSocketHandler]
-
-
-class HttpServerProtocol(QuicConnectionProtocol):
+class MozHttpServerProtocol(HttpServerProtocol):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self._handlers: Dict[int, Handler] = {}
@@ -280,7 +111,7 @@ class HttpServerProtocol(QuicConnectionProtocol):
                     "subprotocols": subprotocols,
                     "type": "websocket",
                 }
-                handler = WebSocketHandler(
+                handler = MozWebSocketHandler(
                     connection=self._http,
                     scope=scope,
                     stream_id=event.stream_id,
@@ -303,7 +134,7 @@ class HttpServerProtocol(QuicConnectionProtocol):
                     "scheme": "https",
                     "type": "http",
                 }
-                handler = HttpRequestHandler(
+                handler = MozHttpRequestHandler(
                     authority=authority,
                     connection=self._http,
                     protocol=self,
@@ -321,39 +152,9 @@ class HttpServerProtocol(QuicConnectionProtocol):
             handler = self._handlers[event.stream_id]
             handler.http_event_received(event)
 
-    def quic_event_received(self, event: QuicEvent) -> None:
-        if isinstance(event, ProtocolNegotiated):
-            if event.alpn_protocol.startswith("h3-"):
-                self._http = H3Connection(self._quic)
-            elif event.alpn_protocol.startswith("hq-"):
-                self._http = H0Connection(self._quic)
-        elif isinstance(event, DatagramFrameReceived):
-            if event.data == b"quack":
-                self._quic.send_datagram_frame(b"quack-ack")
-
-        #  pass event to the HTTP layer
-        if self._http is not None:
-            for http_event in self._http.handle_event(event):
-                self.http_event_received(http_event)
-
-
-class SessionTicketStore:
-    """
-    Simple in-memory store for session tickets.
-    """
-
-    def __init__(self) -> None:
-        self.tickets: Dict[bytes, SessionTicket] = {}
-
-    def add(self, ticket: SessionTicket) -> None:
-        self.tickets[ticket.ticket] = ticket
-
-    def pop(self, label: bytes) -> Optional[SessionTicket]:
-        return self.tickets.pop(label, None)
-
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="QUIC server")
+    parser = argparse.ArgumentParser(description="MozQUIC server")
     parser.add_argument(
         "app",
         type=str,
@@ -365,7 +166,7 @@ if __name__ == "__main__":
         "-c",
         "--certificate",
         type=str,
-        required=True,
+        default=HERE / "ssl_cert.pem",
         help="load the TLS certificate from the specified file",
     )
     parser.add_argument(
@@ -384,7 +185,7 @@ if __name__ == "__main__":
         "-k",
         "--private-key",
         type=str,
-        required=True,
+        default=HERE / "ssl_key.pem",
         help="load the TLS private key from the specified file",
     )
     parser.add_argument(
@@ -445,12 +246,15 @@ if __name__ == "__main__":
     if uvloop is not None:
         uvloop.install()
     loop = asyncio.get_event_loop()
+
+    host = "localhost" if args.host == "::" else args.host
+    logging.info(f"Running on {host}:{args.port}")
     loop.run_until_complete(
         serve(
             args.host,
             args.port,
             configuration=configuration,
-            create_protocol=HttpServerProtocol,
+            create_protocol=MozHttpServerProtocol,
             session_ticket_fetcher=ticket_store.pop,
             session_ticket_handler=ticket_store.add,
             retry=args.retry,
@@ -460,3 +264,5 @@ if __name__ == "__main__":
         loop.run_forever()
     except KeyboardInterrupt:
         pass
+
+
